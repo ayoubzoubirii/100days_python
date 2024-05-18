2156a3ff55d1e8b178422fb550b18c28b84ecf74
ayoubzoubirii <ayoubzoubiriyc@gmail.com>
2024-05-18 16:55:58 +0100

    fuzzbazz

--- 1_to_14_B/Day_5/p10_for_exercice.py
+++ 1_to_14_B/Day_5/p10_for_exercice.py
@@ -81,3 +81,34 @@ for student_score in student_scores :
   if student_score >= top_score :
     top_score = student_score
 print(f"The highest score in the class is: {top_score}")    
+
+
+
+# Instructions
+# You are going to write a program that calculates the sum of all the even numbers from 1 to X. If X is 100 then the first even number would be 2 and the last one is 100:
+
+# i.e. 2 + 4 + 6 + 8 +10 ... + 98 + 100
+
+# Important, there should only be 1 print statement in your console output. It should just print the final total and not every step of the calculation.
+
+# Also, we will constrain the inputs to only take numbers from 0 to a max of 1000.
+
+# Example Input 1
+# 10
+# Example Output 1
+# 30
+# Example Input 2
+# 52
+# Example Output 2
+# 702
+# Hint
+# There are quite a few ways of solving this problem, but you will need to use the range() function in any of the solutions.
+
+target = int(input()) # Enter a number between 0 and 1000
+# 🚨 Do not change the code above ☝️
+
+# Write your code here 👇
+total = 0
+for n in range(0,target+1,2) :
+  total += n
+print(total)
